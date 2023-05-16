2a4741b6ad462e77377e1b12f7baa8e3f8a474fe
Dhruvvats13 <78378557+Dhruvvats13@users.noreply.github.com>
2023-05-16 09:55:47 +0530

    Update Payment.js

--- frontend/src/Payment.js
+++ frontend/src/Payment.js
@@ -1,57 +1,57 @@
 import React, { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import './Payment.css';
 
-const Payment = () => {
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const stripe = useStripe();
-  const elements = useElements();
+function Payment() {
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardExpiry, setCardExpiry] = useState('');
+  const [cardCvv, setCardCvv] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setLoading(true);
-    setErrorMessage(null);
-
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    }); 
-
-    if (error) {
+    if (!cardNumber || !cardExpiry || !cardCvv) {
+      setErrorMessage('Please fill in all fields');
+      return;
+    }
+    const [month, year] = cardExpiry.split('/');
+    if (!/^\d{2}$/.test(month) || !/^\d{2}$/.test(year)) {
+      setErrorMessage('Invalid expiration date');
+      return;
+    }
+    try {
+      const response = await fetch('http://localhost:3001/api/auth/payment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cardNumber,
+          cardExpiry,
+          cardCvv
+        })
+      });
+      console.log(response);
+      // Handle successful payment response
+    } catch (error) {
       setErrorMessage(error.message);
-      setLoading(false);
-    } else {
-      try {
-        const response = await fetch('http://localhost:3001/payment', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ amount: 1000, token: paymentMethod }),
-        });
-        if (response.ok) {
-          // handle successful payment
-        } else {
-          throw new Error('Failed to process payment');
-        }
-      } catch (error) {
-        setErrorMessage(error.message);
-      }
-      setLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Payment Information</h2>
-      <label>
-        Card Information
-        <CardElement />
-      </label>
-      <button type="submit" disabled={!stripe || loading}>
-        {loading ? 'Processing...' : 'Pay'}
-      </button>
-      {errorMessage && <div className="error">{errorMessage}</div>}
-    </form>
+    <div className='Payment'>
+      <div className="auth-form-container">
+        <form className="payment-form" onSubmit={handleSubmit}>
+          <h2>Complete Your Payment</h2>
+          <label>Card Number<input type="text" id="cardNumber" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} /></label><br />
+          <label>Expiration Date<input type="text" id="cardExpiry" value={cardExpiry} onChange={(e) => setCardExpiry(e.target.value)} /></label><br />
+          <label>CVV<input type="text" id="cardCvv" value={cardCvv} onChange={(e) => setCardCvv(e.target.value)} /></label><br />
+
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <button type="submit" className="submit-button">Submit Payment</button>
+        </form>
+      </div>
+    </div>
   );
-};
+}
 
 export default Payment;
