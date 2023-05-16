577cd55a7e489ab1b217b0967d366a3d9c5c1a59
Dhruvvats13 <78378557+Dhruvvats13@users.noreply.github.com>
2023-05-16 10:50:56 +0530

    Update Card.js

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,8 +1,7 @@
-
 import React, { useState, useRef, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatchCart, useCart } from './ContextReducer'
-// import { Dropdown, DropdownButton } from 'react-bootstrap';
+
 export default function Card(props) {
   let data = useCart();
 
@@ -10,83 +9,58 @@ export default function Card(props) {
   const [qty, setQty] = useState(1)
   const [size, setSize] = useState("")
   const priceRef = useRef();
-  // const [btnEnable, setBtnEnable] = useState(false);
-  // let totval = 0
-  // let price = Object.values(options).map((value) => {
-  //   return parseInt(value, 10);
-  // });
   let options = props.options;
   let priceOptions = Object.keys(options);
   let foodItem = props.item;
   const dispatch = useDispatchCart();
-  const handleClick = () => {
-    if (!localStorage.getItem("token")) {
-      navigate("/login")
-    }
-  }
+  
   const handleQty = (e) => {
     setQty(e.target.value);
   }
+  
   const handleOptions = (e) => {
     setSize(e.target.value);
   }
+  
   const handleAddToCart = async () => {
-    let food = []
+    let food = null;
     for (const item of data) {
-      if (item.id === foodItem._id) {
+      if (item.id === foodItem._id && item.size === size) {
         food = item;
-
         break;
       }
     }
-
-    if (food !== []) {
-      if (food.size === size) {
-        await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
-        return
-      }
-      else if (food.size !== size) {
-        await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size,img: props.ImgSrc })
-        console.log("Size different so simply ADD one more to the list")
-        return
-      }
-      return
+    
+    if (food) {
+      await dispatch({ type: "UPDATE", id: foodItem._id, size: size, qty: parseInt(food.qty) + parseInt(qty) })
+    } else {
+      await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: parseInt(options[size]), qty: qty, size: size, img: props.ImgSrc })
     }
-
-    await dispatch({ type: "ADD", id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size })
-
-
-    // setBtnEnable(true)
-
+  }
+  
+  const handleRemoveFromCart = async () => {
+    await dispatch({ type: "REMOVE", id: foodItem._id, size: size })
   }
 
   useEffect(() => {
     setSize(priceRef.current.value)
   }, [])
 
-  // useEffect(()=>{
-  // checkBtn();
-  //   },[data])
-
-  let finalPrice = qty * parseInt(options[size]);   //This is where Price is changing
-  // totval += finalPrice;
-  // console.log(totval)
+  let finalPrice = qty * parseInt(options[size]);
   return (
     <div>
-
       <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
         <img src={props.ImgSrc} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
         <div className="card-body">
           <h5 className="card-title">{props.foodName}</h5>
-          {/* <p className="card-text">This is some random text. This is description.</p> */}
           <div className='container w-100 p-0' style={{ height: "38px" }}>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} onClick={handleClick} onChange={handleQty}>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} onChange={handleQty}>
               {Array.from(Array(6), (e, i) => {
                 return (
                   <option key={i + 1} value={i + 1}>{i + 1}</option>)
               })}
             </select>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} ref={priceRef} onClick={handleClick} onChange={handleOptions}>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" style={{ select: "#FF0000" }} ref={priceRef} onChange={handleOptions}>
               {priceOptions.map((i) => {
                 return <option key={i} value={i}>{i}</option>
               })}
@@ -97,7 +71,7 @@ export default function Card(props) {
           </div>
           <hr></hr>
           <button className={`btn btn-success justify-center ms-2 `} onClick={handleAddToCart}>Add to Cart</button>
-          {/* <button className={`btn btn-danger justify-center ms-2 ${btnEnable ? "" : "disabled"}`} onClick={handleRemoveCart}>Remove</button> */}
+          <button className={`btn btn-danger justify-center ms-2 `} onClick={handleRemoveFromCart}>Remove from Cart</button>
         </div>
       </div>
     </div>
