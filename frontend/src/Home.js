29a3659ce57661ee50b0f5bf68c46fbe91e31ca0
Dhruvvats13 <78378557+Dhruvvats13@users.noreply.github.com>
2023-05-16 10:50:06 +0530

    Update Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,6 +3,7 @@ import Card from './Card'
 // import Carousel from '../components/Carousel'
 import Footer from './Footer'
 import Navbar from './component/Navbar'
+import './Home.css'
 export default function Home() {
   const [foodCat, setFoodCat] = useState([])
   const [foodItems, setFoodItems] = useState([])
@@ -28,40 +29,13 @@ export default function Home() {
   }, [])
 
   return (
-    <div >
+    <div className='Home'>
       <div>
         <Navbar />
       </div>
-      <div>
-        <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
-
-          <div className="carousel-inner " id='carousel'>
-            <div className=" carousel-caption  " style={{ zIndex: "9" }}>
-              <div className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
-                <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
-                <button className="btn text-white bg-danger" onClick={() => { setSearch('') }}>X</button>
-              </div>
+           <div >
+              <img src={require("./delicious-indian-food-tray.jpg")} className=" image" />
             </div>
-            <div className="carousel-item active" >
-              <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
       <div className='container'> {/* boootstrap is mobile first */}
         {
           foodCat !== []
@@ -77,7 +51,7 @@ export default function Home() {
                     (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
                     .map(filterItems => {
                       return (
-                        <div className='col-12 col-md-6 col-lg-3' key={filterItems._id}>
+                        <div className='col-12 col-md-6 col-lg-3' key={filterItems.id}>
                           <Card foodName={filterItems.name} item={filterItems} options={filterItems.options[0]} ImgSrc={filterItems.img} ></Card>
                         </div>
                       )
